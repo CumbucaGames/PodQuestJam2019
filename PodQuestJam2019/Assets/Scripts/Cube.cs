92e90ff2dff62dd716df10257e7406e33c796f87
Dalton <Lima>
2019-09-22 15:25:25 +0200

    Revert last commit (the function wasn't deprecated)
    This reverts commit ab18969c9c791818344da521a6443b6c8dcb75e8.

--- PodQuestJam2019/Assets/Scripts/Cube.cs
+++ PodQuestJam2019/Assets/Scripts/Cube.cs
@@ -1,11 +1,10 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
 
 [RequireComponent(typeof(Transform))]
 public class Cube : MonoBehaviour
 {
     private Transform _transform;
-    [SerializeField] private AnimationCurve rotateCurve;
+    //[SerializeField] private AnimationCurve rotateCurve;
     
     private void Awake()
     {
@@ -14,45 +13,36 @@ public class Cube : MonoBehaviour
 
     public void RotateUp()
     {
-        transform.Rotate(90f, 0, 0, Space.World);
-//        StartCoroutine(Rotate(transform.position, Vector3.left, -90));
+        transform.RotateAround(transform.position, Vector3.left, -90);
+        //StartCoroutine(Rotate(transform.position, Vector3.left, -90));
     }
     
     public void RotateDown()
     {
-        transform.Rotate(-90f, 0, 0, Space.World);
-//        StartCoroutine(Rotate(transform.position, Vector3.left, 90));
+        transform.RotateAround(transform.position, Vector3.left, 90);
+        //StartCoroutine(Rotate(transform.position, Vector3.left, 90));
     }
     
     public void RotateLeft()
     {
-        transform.Rotate(0, 0, 90f, Space.World);
-        //        StartCoroutine(Rotate(transform.position, Vector3.forward, 90));
+        transform.RotateAround(transform.position, Vector3.forward, 90);
+        //StartCoroutine(Rotate(transform.position, Vector3.forward, 90));
     }
     
     public void RotateRight()
-    {
-        transform.Rotate(0, 0, -90f, Space.World);
-        //        StartCoroutine(Rotate(transform.position, Vector3.forward, -90));
+    { 
+        transform.RotateAround(transform.position, Vector3.forward, -90);
+        //StartCoroutine(Rotate(transform.position, Vector3.forward, -90));
     }
 
 //    private IEnumerator Rotate(Vector3 point, Vector3 axis, float angle)
 //    {
-//        // TODO: calculate the rotate pivot point
-//        
-//        var indexLastFrame = rotateCurve.length - 1;
-//        var duration = rotateCurve.keys[indexLastFrame].time;
-//        var finalValue = rotateCurve[indexLastFrame].value;
+//        var length = rotateCurve.length;
 //
-//        var t = 0f;
-//        while (t < duration)
+//        for (int i = 0; i < length; i++)
 //        {
-//            var value = rotateCurve.Evaluate(t);
-//            
-//            var angleStep = Mathf.Lerp(0, angle, (float) value/finalValue);
-//            transform.RotateAround(transform.localPosition, axis, angleStep);
-//            //transform.Rotate(Quaternion.Euler());
-//            t += Time.fixedDeltaTime;
+//            var angleStep = Mathf.Lerp(0, angle, (float) i / length);
+//            transform.RotateAround(transform.position, Vector3.forward, angleStep);
 //            yield return new WaitForFixedUpdate();
 //        }
 //        yield return null;
